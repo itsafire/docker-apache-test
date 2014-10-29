bcf3221324f987ec031de4c76b5fa392d679531e
Andreas Elvers <andreas.githubcontribution@gomorrha.de>
2014-10-29 13:09:23 +0100

    change name of created containers

--- test-single.sh
+++ test-single.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -ti --rm --link redis:redis -e VIRTUAL_HOST=test_single.docker.work.de --name apache_test_single itsafire/apache-test /sbin/my_init -- bash -l
+docker run -ti --rm --link redis:redis -e VIRTUAL_HOST=test-single.docker.work.de --name apache_test_single itsafire/apache-test /sbin/my_init -- bash -l
 
--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ COUNT=10
 case "$1" in
 start)
     for i in $(seq 1 $COUNT); do 
-        docker run --link elk:elk --link redis:redis -d -e VIRTUAL_HOST=test$i.docker.work.de --name test$i.docker.work.de itsafire/apache-test 
+        docker run --link redis:redis -d -e VIRTUAL_HOST=test$i.docker.work.de --name test$i.docker.work.de itsafire/apache-test 
     done
 ;;
 stop)
